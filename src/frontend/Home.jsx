a3f814cceca7bee84cf08f1e1dd44df9ab2e11ba
AmadoJunior <amadoelrepalol@gmail.com>
2024-02-09 11:58:38 -0500

    Implemented file upload form into home component

--- src/frontend/Home.jsx
+++ src/frontend/Home.jsx
@@ -1,7 +1,52 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 export default function Home() {
+	const [file, setFile] = useState()
+
+	const handleFile = (e) => {
+		if (e.target.files?.length) setFile(e.target.files[0])
+	}
+
+	const handleUpload = (e) => {
+		e.preventDefault()
+
+		const formData = new FormData()
+		formData.append('file', file)
+		formData.append('fileName', file.name)
+
+		const axiosConfig = {
+			headers: {
+				'content-type': 'multipart/form-data'
+			}
+		}
+
+		axios.post('/api/upload', formData, axiosConfig)
+			.then((response) => {
+				console.log(response.status, response.data)
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	}
+
 	return (
 		<main>
-			<h1>Welcome to the home page!</h1>
+			<h1>Data Extractor</h1>
+			<div>
+				<h2>Uploaded Files</h2>
+				<ul>
+					<li>File1</li>
+					<li>File2</li>
+					<li>File3</li>
+				</ul>
+			</div>
+			<form onSubmit={handleUpload}>
+				<div className='form-group'>
+					<input type='file' className='form-control-file' onChange={handleFile} />
+					<input type='submit' className='form-control-submit' value='Upload' />
+				</div>
+			</form>
 		</main>
 	)
 }
