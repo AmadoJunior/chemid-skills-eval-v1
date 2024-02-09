0bc41f8d292bdbcf551c2f9608ad576f1fb8f181
AmadoJunior <amadoelrepalol@gmail.com>
2024-02-09 18:00:02 -0500

    Integrated components and hooks into home component

--- src/frontend/Home.jsx
+++ src/frontend/Home.jsx
@@ -1,52 +1,45 @@
-import { useState } from 'react'
-import axios from 'axios'
+// Deps
+import { useEffect, useState } from 'react'
+
+// Components
+import FileUpload from './Components/FileUpload'
+import UploadedFilesList from './Components/UploadedFilesList'
+import Table from './Components/Table'
+
+// Hooks
+import useUploadedFiles from './Hooks/useUploadedFiles'
+import useSelectedFile from './Hooks/useSelectedFile'
 
 export default function Home() {
-	const [file, setFile] = useState()
+	// State
+	const [selectedFileKey, setSelectedFileKey] = useState()
+	const { files, setFiles, firstFileKey } = useUploadedFiles()
+	const { selectedFile } = useSelectedFile(selectedFileKey)
 
-	const handleFile = (e) => {
-		if (e.target.files?.length) setFile(e.target.files[0])
+	// Handlers
+	const handleUploadSuccess = ({ fileName, fileUrl }) => {
+		setFiles((prev) => ({ ...prev, [fileName]: fileUrl }))
+		setSelectedFileKey(fileName)
 	}
 
-	const handleUpload = (e) => {
-		e.preventDefault()
-
-		const formData = new FormData()
-		formData.append('file', file)
-		formData.append('fileName', file.name)
-
-		const axiosConfig = {
-			headers: {
-				'content-type': 'multipart/form-data'
-			}
-		}
-
-		axios.post('/api/upload', formData, axiosConfig)
-			.then((response) => {
-				console.log(response.status, response.data)
-			})
-			.catch((err) => {
-				console.error(err)
-			})
-	}
+	// Effect
+	useEffect(() => {
+		if (!selectedFileKey?.length) setSelectedFileKey(firstFileKey)
+	}, [firstFileKey])
 
 	return (
 		<main>
 			<h1>Data Extractor</h1>
-			<div>
-				<h2>Uploaded Files</h2>
-				<ul>
-					<li>File1</li>
-					<li>File2</li>
-					<li>File3</li>
-				</ul>
-			</div>
-			<form onSubmit={handleUpload}>
-				<div className='form-group'>
-					<input type='file' className='form-control-file' onChange={handleFile} />
-					<input type='submit' className='form-control-submit' value='Upload' />
-				</div>
-			</form>
+			<FileUpload onUploadSuccess={handleUploadSuccess} />
+			<UploadedFilesList files={files} onSelectFile={setSelectedFileKey} />
+			{selectedFile && (
+				<>
+					<h4>Parsed Values</h4>
+					<Table fileName={selectedFile.fileName} fileBody={selectedFile.fileBody} />
+					<h4>Computed Density</h4>
+					<Table fileName={selectedFile.fileName} fileBody={selectedFile.computedValues} />
+				</>
+			)}
 		</main>
 	)
 }
