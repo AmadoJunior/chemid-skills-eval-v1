c19672f77872c0fcf0bc78ffd85edcb8e4f10de4
AmadoJunior <amadoelrepalol@gmail.com>
2024-02-09 11:59:53 -0500

    Implemented base express entry file and added upload route

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
-const port = 8080
+const env = require('dotenv')
+const path = require('path')
 
+//Init
+env.config({path: path.join(__dirname, '..', '..', '.env')})
 const app = express()
 
-app.listen(port, () => {
+//Middleware
+app.use(express.json())
+
+//Routes
+const upload = require("./routes/upload.js");
+app.use("/api/upload", upload)
+
+app.get("/", (req, res) => {
+	res.status(200).send("OK")
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
 	console.log('-----------------------------------')
-	console.log(`SERVER STARTED AND LISTENING ON PORT ${port}`)
+	console.log(`SERVER STARTED AND LISTENING ON PORT ${PORT}`)
 })
